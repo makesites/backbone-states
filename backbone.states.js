0e16535d1a56aa418adfb0ca1853b9d0380c3628
Makis Tracend <makis.tracend@gmail.com>
2013-10-02 15:19:09 -0700

    initializing states

---
+++ backbone.states.js
@@ -0,0 +1,35 @@
+/*
+ * Backbone.ready()
+ * Source: https://gist.github.com/tracend/5617079
+ *
+ * Created by Makis Tracend ( [@tracend](http://github.com/tracend) )
+ * Released under the [MIT license](http://makesites.org/licenses/MIT)
+ *
+ */
+(function(window, document, Backbone){
+
+	// find the $
+	//$ = ('$' in window) ? window.$ : window.jQuery || window.Zepto || false;
+
+	Backbone.View = Backbone.View.extend({
+		states: {
+		}, 
+		initialize: function(options){
+			this.initStates();
+			return Backbone.View.prototype.initialize.call(this, options);
+		}, 
+		initStates: function(){
+			for(var e in this.states){
+				var method = this.states[e];
+				this.bind(e, _.bind(this[method], this) );
+			}
+		}
+
+	});
+
+	// Helpers
+
+
+	return Backbone;
+
+})(window, document, this.Backbone);
